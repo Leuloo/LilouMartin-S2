bcc78471d9ce92dd5aef519be3201bf89a8fea01
Leuloo <150129180+Leuloo@users.noreply.github.com>
2025-07-19 16:53:07 +0200

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -27,7 +27,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
               <img
  src="/logo.svg"
    alt="Logo Colorlab"
-  className="object-contain"
+  className="w-30 h-30 object-contain"
                 />
             </Link>
 
